11dcf9f8d9305a5f98c20be90e302f1241634f9c
Seulkiming <conductor@tutti.kr>
2014-07-31 23:24:19 +0900

    so many changes

--- app/views.py
+++ app/views.py
@@ -1,8 +1,7 @@
 #-*-coding:utf-8-*-
 from flask import render_template, request, redirect, url_for, current_app
-from googledb import Files
-from app import app
 from uploadpic import Picture
+from app import app
 
 import urllib2
 import sys
@@ -16,8 +15,7 @@ def index():
     return render_template("index.html")
 
 @app.route("/test_picdata", methods=["GET", "POST"])
-
-def test_picdata():
+def picdata():
     if request.method == "POST":
         file = request.files.get("file")
         filestream = file.read()
@@ -25,31 +23,38 @@ def test_picdata():
         picture.setPicture(filestream)
         picture.setMetadata({})
         picture.put()
-        return "Great!"
+        return redirect(url_for("display_pic", key=picture.key()))
     else:
        return render_template("test_picdata.html")
 
-@app.route("/update", methods=["GET", "POST"])
-def db_update():
-    if request.files:
-        post_data = request.files.get("photo")
-        string_data = request.form.get("string")
-
-        filestream = post_data.read()
-
-        upload_data = Files()
-        upload_data.photo = db.Blob(filestream)
-        upload_data.string = string_data
-        upload_data.put()
-
-        url = url_for("display_image", key=upload_data.key())
- 
-        result = Files.all()
-
-        return render_template("minitwitter.html", url=url, string_data=string_data, result=result)
-    return render_template("minitwitter.html")
-
 @app.route("/show/<key>", methods=["GET"])
-def display_image(key):
-    upload_data = db.get(key)
+def display_pic(key):
+    uploadedPic = db.get(key)
+
+    picInfo = {}
+    picInfo["picURL"] = url_for("img", key=key)
+    picInfo["picMetadata"] = uploadedPic.getMetadata()
     return current_app.response_class(upload_data.photo)
+
+
+
+# @app.route("/update", methods=["GET", "POST"])
+# def db_update():
+#     if request.files:
+#         post_data = request.files.get("photo")
+#         string_data = request.form.get("string")
+
+#         filestream = post_data.read()
+
+#         upload_data = Files()
+#         upload_data.photo = db.Blob(filestream)
+#         upload_data.string = string_data
+#         upload_data.put()
+
+#         url = url_for("display_image", key=upload_data.key())
+ 
+#         result = Files.all()
+
+#         return render_template("minitwitter.html", url=url, string_data=string_data, result=result)
+#     return render_template("minitwitter.html")
+# # 
